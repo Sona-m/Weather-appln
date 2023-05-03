0eb9d1b37c3c19b5d7944189073fcdb4a991e0f3
Sonam Kumari <sonamkri1605@gmail.com>
2023-05-03 21:11:21 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "4bd75d5be0b656f3e80f79055561fdde",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
